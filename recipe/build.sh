3dd18b3c795b5408728111dc53a748704f00534a
UENO, M. <eunosium-1128@live.jp>
2026-03-13 15:14:51 +0900

    Enhance build.sh for cross-compilation support
    Add support for cross-compilation targets in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+if [[ ${build_platform} != ${target_platform} ]]; then
+    case ${target_platform} in
+        linux-aarch64)
+            cargo_target_args="-DRust_CARGO_TARGET=aarch64-unknown-linux-gnu"
+            ;;
+        linux-ppc64le)
+            cargo_target_args="-DRust_CARGO_TARGET=powerpc64le-unknown-linux-gnu"
+            ;;
+        osx-arm64)
+            cargo_target_args="-DRust_CARGO_TARGET=aarch64-apple-darwin"
+            ;;
+        *)
+            echo "Unsupported cross-compilation target: ${target_platform}"
+            exit 1
+            ;;
+    esac
+fi
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 cmake -S . -B build \
     ${CMAKE_ARGS} \
-  -DSYSTEM_CORROSION=ON
+  -DSYSTEM_CORROSION=ON \
+  ${cargo_target_args}
+
 cmake --build build --parallel ${CPU_COUNT}
 ctest -V --test-dir build --parallel ${CPU_COUNT}
 cmake --install build
